e5f43eb0f3544f1a6a4cf529429580e222e88787
Alain Puron <alain.1puron@gmail.com>
2025-11-10 14:41:21 -0700

    movement

---
+++ scenes/character/character.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D # the script can use the properties of
+
+
+@export var animation: Node
+
+var _velocity: float = 100.0
+var _jump_velocity: float = -300.0
+
+#	func _process(delta):
+	# position.x += 1 -- Character would move depending on the PC's power
+		#position.x += 10 * delta # character moves 1 frame per second
+
+# Runs 60 times per second 
+func _physics_process(delta):
+	
+	
+	
+	#gravity
+	velocity += get_gravity() * delta # return the gravity value
+	
+	#jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += _jump_velocity
+		
+	# movement 
+	if Input.is_action_pressed("right"):
+		velocity.x = _velocity
+		animation.flip_h = true
+	elif Input.is_action_pressed("left"):
+		velocity.x =-_velocity
+		animation.flip_h = false
+
+	else:
+		velocity.x = 0
+	move_and_slide()
+	
+	# Animation
+	if !is_on_floor():
+		animation.play("jumping")
+	elif velocity.x !=0:
+		animation.play("running")
+	else:
+		animation.play("idle")
