99eead69dd2ebcc72c3b0b01a377e9f0d0270828
Alain Puron <alain.1puron@gmail.com>
2025-11-10 21:43:34 -0700

    init dead

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -2,17 +2,27 @@ extends CharacterBody2D # the script can use the properties of
 
 
 @export var animation: AnimatedSprite2D
+@export var area_2d: Area2D
+@export var character_red_material: ShaderMaterial
 
 var _velocity: float = 100.0
 var _jump_velocity: float = -300.0
+var _dead: bool = false
 
 #	func _process(delta):
 	# position.x += 1 -- Character would move depending on the PC's power
 		#position.x += 10 * delta # character moves 1 frame per second
 
+
+func _ready():
+	area_2d.body_entered.connect(_on_area_2d_body_entered)
+	
+	
+	
 # Runs 60 times per second 
 func _physics_process(delta):
-	
+	if _dead:
+		return
 	
 	
 	#gravity
@@ -41,3 +51,9 @@ func _physics_process(delta):
 		animation.play("running")
 	else:
 		animation.play("idle")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	animation.material = character_red_material # or animation.module = Color.(18.892,0.0,0.0,1.0)
+	_dead = true
+	animation.stop()
