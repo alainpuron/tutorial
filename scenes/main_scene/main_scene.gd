04696b99d5918916590469950108fad6b5fd9980
Alain Puron <alain.1puron@gmail.com>
2025-11-11 21:36:49 -0700

    reset level

--- scenes/main_scene/main_scene.gd
+++ scenes/main_scene/main_scene.gd
@@ -15,6 +15,14 @@ func _create_level(level_number: int):
 	_level_instantiate = levels[level_number - 1].instantiate()
 	add_child(_level_instantiate)
 	var children := _level_instantiate.get_children()
+	
+	#var characters := get_tree().get_nodes_in_group("characters")
+	#characters[0].dead_character.connect(_reset_level)
+	
+	for i in children.size():
+		if children[i].is_in_group("characters"):
+			children[i].dead_character.connect(_restart_level)
+			break
 
 func _delete_level():
 	_level_instantiate.queue_free()
